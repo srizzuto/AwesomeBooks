dcddce766ac982e1344cfb44a64e74db35d46830
Sebastian Rizzuto <srizzuto@gmail.com>
2021-08-16 10:49:44 -0300

    create add and remove methods

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,31 @@ class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
+    console.log('A Book was created')
   }
 }
 
+class Library {
+  constructor() {
+    self = this;
+    self.books = [];
+    this.booklist = document.getElementById('booklist');
+    document.getElementById('add').addEventListener('click', this.add)
+    console.log('Library created')
+  }
+
+  add() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let book = new Book(title, author);
+    self.books.push(book);
+    console.log(self.books);
+    console.log(`"${book.title}" by ${book.author} was created`);
+  }
+
+  remove() {
+    self.books = this.books.filter((book) => self.books.indexOf(book));
+  }
+}
+
+const library = new Library();
